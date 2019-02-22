af0c5864c94f910ffdd6e9ec1c7f8e0bdfb97348
flowarina <flowarina@gmail.com>
2019-02-22 12:36:55 +0300

    first week

---
+++ src/main/java/Sample.java
@@ -0,0 +1,335 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Task 1.4)");
+        System.out.println(" ");
+
+
+        System.out.print("Task 1.5)");
+        System.out.println(" ");
+        System.out.print(1);
+        System.out.print(" ");
+        System.out.print(13);
+        System.out.print(" ");
+        System.out.print(49);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.6)");
+        System.out.println(" ");
+        System.out.print(7);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(15);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(100);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.7)");
+        System.out.println(" ");
+        System.out.print(1);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(2);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(3);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.8)");
+        System.out.println(" ");
+        System.out.print(10);
+        System.out.print(" ");
+        System.out.print(11);
+        System.out.print(" ");
+        System.out.print(12);
+        System.out.print(" ");
+        System.out.print(13);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.9)");
+        System.out.println(" ");
+        System.out.println(50);
+        System.out.print(10);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.10)");
+        System.out.println(" ");
+        System.out.println(5);
+        System.out.println(10);
+        System.out.print(21);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.11)");
+        System.out.println(" ");
+        System.out.println(30);
+        System.out.println(31);
+        System.out.println(32);
+        System.out.print(33);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.12)");
+        System.out.println(" ");
+        System.out.print("a)5 10");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("б)100 t");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("в)x 25");
+        System.out.println(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("7 см");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("1949 v");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print("x y");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.13)");
+        System.out.println(" ");
+        System.out.print("а)2 кг");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("б)а 1");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("в)x y");
+        System.out.println(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("13 17");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print("19 b");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("5 y");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.14)");
+        System.out.println(" ");
+        System.out.print("a)2x;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("д)|n|;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("и)sinαcosβ+cosαsinβ;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.print("б)sin x;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("e)5 cos y;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("k)a√2b;");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("в)a²;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("ж)-7,5a²;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("л)3sin2αcos3β;");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("г)√x;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("г)3√x;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("м)-5√x+√y;");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.15)");
+        System.out.println(" ");
+        System.out.print("a)-1/x²;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("г)(a+b)/2;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("ж)(b+(1/a)/(2/c);");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("б)(a/b)/c;");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("д)5,45*(a+2b)/(2-a);");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("з)1+(a+b/2)/1;");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("в)(a/b)/c;");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("е)-b+(√b²-4ac)/2a;");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("и)1/(1+1/1)/(2+((1/(2+3/5)));");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.18)");
+        System.out.println(" ");
+        System.out.print("a)s=-52");
+        System.out.print(" ");
+        System.out.print("в)s=-15");
+        System.out.println(" ");
+        System.out.print("б)s=6");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("г)s=20");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.19)");
+        System.out.println(" ");
+        System.out.print("a)х=0");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("в)х=-59");
+        System.out.println(" ");
+        System.out.print("б)х=35");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("г)х=2");
+
+        System.out.print("Task 1.20)");
+        System.out.println(" ");
+        System.out.print("a)k=30");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("a)s=-50");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Task 1.21)");
+        System.out.println(" ");
+        System.out.print("a)b=5.8");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("a=5.8");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("б)b=0");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("a=0");
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class SampleTest {
+
+
+
+    @Test
+    public void testGetFive() {
+        Assert.assertEquals(5,Sample.getFive());
+        Assert/assertEquals()
+    }
+
+}

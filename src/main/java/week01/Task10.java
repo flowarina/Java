b9408d894f284d2f4f338f63775df4d91eeecf44
flowarina <47871716+flowarina@users.noreply.github.com>
2019-02-25 17:30:59 +0300

    Update Task10.java

--- src/main/java/week01/Task10.java
+++ src/main/java/week01/Task10.java
@@ -2,9 +2,7 @@ package week01;
 
 public class Task10 {
     public static void main(String[] args) {
-        System.out.println("5");
-        System.out.println("10");
-        System.out.println("21");
+        System.out.println("5\n10\n21");
     }
     public static String getNumbers(){
         return "5 10 21";

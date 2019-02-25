90d874977d459e8fcf2666eae4923dd184996b0f
flowarina <47871716+flowarina@users.noreply.github.com>
2019-02-25 16:23:01 +0300

    Update Task04.java

--- src/main/java/week01/Task04.java
+++ src/main/java/week01/Task04.java
@@ -1,6 +1,10 @@
 package week01;
 
 public class Task04 {
-
-
+    public static void main(String[] args) {
+        System.out.println(Task04.getNumbers());
+    }
+    public static String getNumbers() {
+        return "1   2   3   4";
+    }
 }

4fc6ce859b73c716c3b5c1e893be3092a5afae2f
flowarina <47871716+flowarina@users.noreply.github.com>
2019-02-26 15:47:10 +0300

    Update Task03.java

--- src/main/java/week01/Task03.java
+++ src/main/java/week01/Task03.java
@@ -2,7 +2,7 @@ package week01;
 
 public class Task03 {
     public static void main(String[] args) {
-        System.out.println("0\n35\n-59\n2");
+        System.out.println(Task03.getNumbers());
     }
     public static String getNumbers(){
         return "0 35 -59 2";
